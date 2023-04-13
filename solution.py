c7b794f28cafff2ce8d3d4112f319d0e9f10e592
LisaShtokolova <86804154+LisaShtokolova@users.noreply.github.com>
2023-04-13 22:46:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 854074776 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+  null_x = x_success * 100/x_cnt
+  stat, pval = proportions_ztest(count = y_success, nobs=  x_success, value= null_x)
+
+
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return pval > 0.07 # Ваш ответ, True или False
